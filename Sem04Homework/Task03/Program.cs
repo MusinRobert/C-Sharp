de2e53d3cf90c43b1a6a5bfb041c9ca4d8c04c00
Robert <otdelniiadres@gmail.com>
2023-09-11 22:20:50 +0500

    add Homework seminar 04

---
+++ Sem04Homework/Task03/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] FillArray(int size, int min = 0, int max = 1)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    System.Console.Write("]");
+}
+
+int[] arr = FillArray(8, 10, 100);
+PrintArray(arr);
--- Seminar04/Task02/Program.cs
+++ Seminar04/Task02/Program.cs
@@ -1,33 +1,18 @@
-﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-// 452 -> 11
-// 82 -> 10
-// 9012 -> 12
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 452 -> 3
+// 82 -> 2
+// 9012 -> 4
 
+int num = GetNumber("");
+int i = 0;
 int GetNumber(string message)
 {
     Console.Write($"Введите число {message}: ");
     int num = int.Parse(Console.ReadLine());
     return num;
 }
-int num = GetNumber("");
-int sum = 0;
-while (num != 0)
+for ( ; num > 0; num = num/10)
 {
-    sum += num % 10;
-    num /= 10;
+    i++;
 }
-System.Console.WriteLine(sum);
-
-// int num = GetNumber("");
-// int i = 0;
-// int GetNumber(string message)
-// {
-//     Console.Write($"Введите число {message}: ");
-//     int num = int.Parse(Console.ReadLine());
-//     return num;
-// }
-// for ( ; num > 0; num = num/10)
-// {
-//     i++;
-// }
-// System.Console.WriteLine("Количество цифр в числе " + i);
+System.Console.WriteLine("Количество цифр в числе " + i);
